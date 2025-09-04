2cd1e38eab36250bc5d4df38e2d9ad1bf2072a98
Guillaume Grandjean <101862705+GrandjeanG@users.noreply.github.com>
2025-09-04 16:34:12 +0200

    Initiated dApp (#5)
    Co-authored-by: Guillaume Grandjean <ggrandjean@vaudoise.ch>

---
+++ dapp/src/app/page.tsx
@@ -0,0 +1,22 @@
+// import { cookieStorage, createStorage, http } from '@wagmi/core'
+import { ConnectButton } from "@/components/ConnectButton";
+import { InfoList } from "@/components/InfoList";
+import { ActionButtonList } from "@/components/ActionButtonList";
+import Image from 'next/image';
+
+export default function Home() {
+
+  return (
+    <div className={"pages"}>
+      <Image src="/reown.svg" alt="Reown" width={150} height={150} priority />
+      <h1>Tickethic</h1>
+
+      <ConnectButton />
+      <ActionButtonList />
+      <div className="advice">
+        <p>Welcome on the Tickethic dApp!</p>
+      </div>
+      <InfoList />
+    </div>
+  );
+}

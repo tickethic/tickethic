9a0d03faee2100b3abaf16e90784e6722d29ffd7
dadamagouil <vujica.dario@gmail.com>
2025-09-08 17:20:24 +0200

    feat: Add new functions to Artist contract for listing artists
    - Add getAllArtistIds() to get all artist IDs
    - Add getArtistName() to get artist name by ID
    - Add getTotalArtists() to get total number of artists
    - Add getArtistDetails() to get complete artist info
    - Fix _exists() function calls with proper validation
    - Redeploy all contracts on Polygon Amoy with new addresses

--- evm/contracts/Artist.sol
+++ evm/contracts/Artist.sol
@@ -32,4 +32,50 @@ contract Artist is ERC721URIStorage, Ownable {
         ArtistInfo storage info = artistInfos[artistId];
         return (info.name, info.metadataURI);
     }
+
+    /**
+     * @dev Get all artist IDs that have been minted
+     * @return Array of all artist IDs
+     */
+    function getAllArtistIds() external view returns (uint256[] memory) {
+        uint256 totalArtists = nextArtistId - 1;
+        uint256[] memory allArtists = new uint256[](totalArtists);
+        
+        for (uint256 i = 1; i < nextArtistId; i++) {
+            allArtists[i - 1] = i;
+        }
+        
+        return allArtists;
+    }
+
+    /**
+     * @dev Get artist name by ID
+     * @param artistId The artist ID
+     * @return Artist name
+     */
+    function getArtistName(uint256 artistId) external view returns (string memory) {
+        require(artistId > 0 && artistId < nextArtistId, "Artist does not exist");
+        return artistInfos[artistId].name;
+    }
+
+    /**
+     * @dev Get total number of artists minted
+     * @return Total number of artists
+     */
+    function getTotalArtists() external view returns (uint256) {
+        return nextArtistId - 1;
+    }
+
+    /**
+     * @dev Get artist info by ID (name and owner address)
+     * @param artistId The artist ID
+     * @return name Artist name
+     * @return owner Artist owner address
+     * @return metadataURI Artist metadata URI
+     */
+    function getArtistDetails(uint256 artistId) external view returns (string memory name, address owner, string memory metadataURI) {
+        require(artistId > 0 && artistId < nextArtistId, "Artist does not exist");
+        ArtistInfo storage info = artistInfos[artistId];
+        return (info.name, ownerOf(artistId), info.metadataURI);
+    }
 }
